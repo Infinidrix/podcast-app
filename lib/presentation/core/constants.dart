2e95d1c98f645fca39f85beb69d259503c14a8d7
Hanan Miftah <se.hanan.miftah@gmail.com>
2021-08-15 13:21:24 +0300

    adding some constant widgets on the core package of constants.dart file and finished the edit profile page

--- lib/presentation/core/constants.dart
+++ lib/presentation/core/constants.dart
@@ -1,30 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
-  final textfieldDecoration = InputDecoration(
-    border: UnderlineInputBorder(
-      borderSide: BorderSide(
-        color: Colors.grey,
-      ),
+final textfieldDecoration = InputDecoration(
+  border: UnderlineInputBorder(
+    borderSide: BorderSide(
+      color: Colors.grey,
     ),
-    // labelText: "Email",
-    // labelStyle: TextStyle(color: Colors.white),
-    helperStyle: TextStyle(color: Colors.white),
-    hintText: 'email',
-    hintStyle: TextStyle(color: Colors.white),
-    // filled: true,
-    enabledBorder: UnderlineInputBorder(
-      borderSide: BorderSide(
-        color: Colors.grey,
-      ),
+  ),
+  // labelText: "Email",
+  // labelStyle: TextStyle(color: Colors.white),
+  helperStyle: TextStyle(color: Colors.white),
+  hintText: 'email',
+  hintStyle: TextStyle(color: Colors.white),
+  // filled: true,
+  enabledBorder: UnderlineInputBorder(
+    borderSide: BorderSide(
+      color: Colors.grey,
     ),
-    focusedBorder: UnderlineInputBorder(
-      borderSide: BorderSide(
-        color: Colors.grey,
-      ),
+  ),
+  focusedBorder: UnderlineInputBorder(
+    borderSide: BorderSide(
+      color: Colors.grey,
     ),
-    fillColor: Colors.white,
-    prefixIcon: Icon(
-      Icons.email_outlined,
-      color: Colors.white,
+  ),
+  fillColor: Colors.white,
+  prefixIcon: Icon(
+    Icons.email_outlined,
+    color: Colors.white,
+  ),
+);
+
+final buttonDecoration = BoxDecoration(
+  gradient: LinearGradient(
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      colors: [HexColor("#579EB5"), HexColor("#8EB9C7")]),
+  borderRadius: BorderRadius.circular(20.0),
+);
+
+final buttonStyle = ButtonStyle(
+  backgroundColor: MaterialStateProperty.all(Colors.transparent),
+  shape: MaterialStateProperty.all(
+    RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(20.0),
     ),
-  );
+  ),
+);
--- lib/presentation/pages/edit_profile/widgets/edit_profile_widget.dart
+++ lib/presentation/pages/edit_profile/widgets/edit_profile_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:podcast_app/presentation/core/constants.dart';
 import 'package:podcast_app/presentation/pages/edit_profile/widgets/profile_picture.dart';
+import 'package:podcast_app/presentation/routes/router.gr.dart';
 
 class EditProfileWidget extends StatefulWidget {
   const EditProfileWidget({Key? key}) : super(key: key);
@@ -12,12 +14,113 @@ class EditProfileWidget extends StatefulWidget {
 class _EditProfileWidgetState extends State<EditProfileWidget> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ChangeProfilePricture(),
-        Text("User Name"),
-        
-      ],
+    final _formKey = GlobalKey<FormState>();
+
+    Widget username = TextFormField(
+      style: TextStyle(color: Colors.white),
+      decoration: textfieldDecoration.copyWith(
+        hintText: "John",
+        prefixIcon: Icon(
+          Icons.person_outline,
+          color: Colors.white,
+        ),
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return "This field can't be empty";
+        }
+        return null;
+      },
+    );
+
+    Widget emailInput = Padding(
+      padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 45.0),
+      child: TextFormField(
+        style: TextStyle(color: Colors.white),
+        decoration: textfieldDecoration,
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return "This field can't be empty";
+          } else if (!value.contains("@") || !value.contains(".com")) {
+            return "Invalid email";
+          }
+          return null;
+        },
+      ),
+    );
+
+    Widget cancelButton = DecoratedBox(
+      decoration: buttonDecoration,
+      child: ElevatedButton(
+        style: buttonStyle,
+        onPressed: () {
+          // checking if the form submitted is valid
+          if (_formKey.currentState!.validate()) {
+            print("Successfully cancelled");
+          }
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Container(
+            child: Text("Cancel"),
+          ),
+        ),
+      ),
+    );
+
+    Widget saveButton = DecoratedBox(
+      decoration: buttonDecoration,
+      child: ElevatedButton(
+        style: buttonStyle,
+        onPressed: () {
+          // checking if the form submitted is valid
+          if (_formKey.currentState!.validate()) {
+            print("Successfully saved");
+          }
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Container(
+            child: Text("Save"),
+          ),
+        ),
+      ),
+    );
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 47.5),
+      child: Container(
+        child: Form(
+            child: ListView(
+          children: [
+            ChangeProfilePricture(),
+            Padding(
+              padding: const EdgeInsets.only(top: 73.3),
+              child: Text(
+                "Username",
+                style: TextStyle(color: Colors.grey),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 33.3),
+              child: username,
+            ),
+            Text(
+              "Email",
+              style: TextStyle(color: Colors.grey),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 33.0),
+              child: emailInput,
+            ),
+            cancelButton,
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: saveButton,
+            )
+          ],
+        )),
+      ),
     );
   }
 }

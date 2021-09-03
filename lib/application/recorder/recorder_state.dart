8dcfd2e5efd3cd7edd121360a4dfeab7c88d2fa2
Natneam <se.natneam.mesele@gmail.com>
2021-09-03 15:52:19 +0300

    creating the basic starture of events, states and Bloc

---
+++ lib/application/recorder/recorder_state.dart
@@ -0,0 +1,9 @@
+abstract class RecorderState {}
+
+class InitialState extends RecorderState {}
+
+class RecordState extends RecorderState {}
+
+class PauseState extends RecorderState {}
+
+class DialogState extends RecorderState {}

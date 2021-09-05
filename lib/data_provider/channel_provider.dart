5a5eba62848e7dd0f18ba80bfbb5b6f2d46b0e7e
Hanan Miftah <se.hanan.miftah@gmail.com>
2021-09-05 09:30:16 +0300

    updating the channel  repository

--- lib/data_provider/channel_provider.dart
+++ lib/data_provider/channel_provider.dart
@@ -1,7 +1,16 @@
 import 'package:podcast_app/data_provider/Ichannel_provider.dart';
 import 'package:podcast_app/models/Channel.dart';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dartz/dartz.dart';
+import 'package:http/http.dart' as http;
 
 class ChannelPorvider implements IChannelProvider {
+  final http.Client httpClient;
+
+  ChannelPorvider({required this.httpClient});
+
   List<Podcast> podcasts =
       List.generate(10, (index) => Podcast("Name #$index", index));
   Channel channel = Channel(
@@ -33,15 +42,31 @@ class ChannelPorvider implements IChannelProvider {
     return isSubscribedValue;
   }
 
-  Future<void>? createChannel(Channel channell) {
-    Channel channel = Channel(
-        Name: channell.Name,
-        ImageUrl: channell.ImageUrl,
-        Subscribers: channell.Subscribers,
-        Id: channell.Id,
-        Description: channell.Description);
+  @override
+  Future<Either<String, Channel>> createChannel({required Map<String, dynamic> createChannelInfo}) async{
+      try{
+        final response = await httpClient.post(Uri.http(URL, "/api/createChannel"),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(createChannelInfo),
+        );
+        if (response.statusCode == 200) {
+        final parsed = json.decode(response.body);
+        print(parsed);
+        return right(Channel.fromJson(parsed));
+      } else if (response.statusCode == 400) {
+        final parsed = json.decode(response.body);
+        return left(parsed["message"]);
+      } else if (response.statusCode == 404) {
+        return left("Can not connect to internet");
+      }
+      return left("Some Error Happened");
+    } on SocketException catch (e) {
+      print(e.message);
+      return left("Can not connect to internet ${e.runtimeType}");
+    }
+        }
 
-    print("Channel created");
-    // return true;
   }
 }

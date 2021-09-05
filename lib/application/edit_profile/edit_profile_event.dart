5a7ac744fa9964256793a4473238070f2c41b535
Abdi <se.abdulfeta.dedgba@gmail.com>
2021-09-05 21:43:17 +0300

    bloc part for edit profile

---
+++ lib/application/edit_profile/edit_profile_event.dart
@@ -0,0 +1,31 @@
+// import 'package:image_picker/image_picker.dart';
+part of 'edit_profile_bloc.dart';
+
+@immutable
+abstract class EditProfileEvent {}
+
+class SaveButtonPressedEditProfileEvent extends EditProfileEvent {
+  final String UserName, Email, Password, ProfilePicture;
+
+  SaveButtonPressedEditProfileEvent(
+      {required this.UserName,
+      required this.Email,
+      required this.Password,
+      required this.ProfilePicture});
+}
+
+class IntitalEditProfileEvent extends EditProfileEvent {}
+
+class PasswordShowButtonPressedEditProfileEvent extends EditProfileEvent {
+  final bool isPassword;
+
+  PasswordShowButtonPressedEditProfileEvent({required this.isPassword});
+}
+
+class ChangeProfileImageButtonPressed extends EditProfileEvent {}
+
+class OpenImagePickerEvent extends EditProfileEvent {
+  final ImageSource imageSource;
+
+  OpenImagePickerEvent({required this.imageSource});
+}

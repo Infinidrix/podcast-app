50a58cd77fe727b1c91bb8433ebcd9103880d6cd
Hanan Miftah <se.hanan.miftah@gmail.com>
2021-09-06 11:28:05 +0300

    event for imagepicker

--- lib/application/create_channel/create_channel_event.dart
+++ lib/application/create_channel/create_channel_event.dart
@@ -1,4 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:podcast_app/models/channel/Channel.dart';
 
 abstract class CreateChannelEvent {}
@@ -9,4 +10,11 @@ class CreateChannelSaveEvent extends CreateChannelEvent {
   final String ImageURL;
 
   CreateChannelSaveEvent({required this.Name, required this.Description, required this.ImageURL});
+}
+ class ChangeProfileImageButtonPressed extends CreateChannelEvent {}
+
+class OpenImagePickerEvent extends CreateChannelEvent {
+  final ImageSource imageSource;
+
+  OpenImagePickerEvent({required this.imageSource});
 }

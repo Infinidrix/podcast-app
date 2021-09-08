35364eea32eb8c0354feba785825a2437980d353
Hanan Miftah <se.hanan.miftah@gmail.com>
2021-09-08 19:04:27 +0300

    maping events eith its corresponding events

---
+++ lib/application/edit_channel_detail/edit_channel_detail_bloc.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:podcast_app/application/edit_channel_detail/edit_channel_detail_event.dart';
+import 'package:podcast_app/application/edit_channel_detail/edit_channel_detail_state.dart';
+import 'package:podcast_app/repository/Ichannel_repository.dart';
+
+class EditChannelDetailBloc
+    extends Bloc<EditChannelDetailEvent, EditChannelDetailState> {
+  IChannelRepository channelRepository;
+  EditChannelDetailBloc({required this.channelRepository}):super(EditChannelDetailInitialState());
+
+  @override
+  Stream<EditChannelDetailState> mapEventToState(EditChannelDetailEvent event) async*{
+    if (event is EditChannelChangeImageButtonPressedEvent) {
+      yield EditChannelDetailImageUploadedState(Image: event.Image);
+    }
+
+    
+  }
+}

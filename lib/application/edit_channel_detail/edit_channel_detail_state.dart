c9a8029b2ccdf3a8f9b93e92954fd5d7591e9a84
Hanan Miftah <se.hanan.miftah@gmail.com>
2021-09-08 18:14:28 +0300

    creating states for edit channel detail page

---
+++ lib/application/edit_channel_detail/edit_channel_detail_state.dart
@@ -0,0 +1,9 @@
+abstract class EditChannelDetailState {}
+
+class EditChannelDetailInitialState extends EditChannelDetailState {}
+
+class EditChannelDetailSuccessState extends EditChannelDetailState {}
+
+class EditChannelDetailImageUploadedState extends EditChannelDetailState {}
+
+class EditChannelDetailFailedState extends EditChannelDetailState {}

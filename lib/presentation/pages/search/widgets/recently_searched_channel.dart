36d31e07493d2f66784e33b545224293ea45a699
Amanuel Debebe <amanueldebebe1000@gmail.com>
2021-09-08 18:11:01 +0300

    Fix RecentlySearched Navigation

---
+++ lib/presentation/pages/search/widgets/recently_searched_channel.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:auto_route/auto_route.dart';
+
+import 'package:podcast_app/application/channel_description/channel_description_bloc.dart';
+import 'package:podcast_app/models/channel/Channel.dart';
+import 'package:podcast_app/presentation/pages/search/widgets/custom_text.dart';
+import 'package:podcast_app/presentation/routes/router.gr.dart';
+
+class RecentlyChannel extends StatelessWidget {
+  final Channel channel;
+
+  const RecentlyChannel({Key? key, required this.channel}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final channelDetailBloc = BlocProvider.of<ChannelDescriptionBloc>(context);
+    return InkWell(
+      onTap: () {
+        channelDetailBloc.add(LoadInitialEvent(channel: channel));
+        context.router.push(ChannelDetailRoute());
+      },
+      child: Card(
+        elevation: 20,
+        color: HexColor("#282828"),
+        child: ListTile(
+          dense: true,
+          leading: CircleAvatar(
+            backgroundImage: AssetImage(channel.ImageUrl),
+            radius: 25,
+          ),
+          title: CustomText(text: "$channel.Name"),
+          subtitle: CustomText(text: "$channel.Description"),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.clear,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/presentation/pages/search/widgets/recently_searched_widget.dart
+++ lib/presentation/pages/search/widgets/recently_searched_widget.dart
@@ -1,41 +1,48 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:podcast_app/application/audio_player/audio_player_bloc.dart';
+import 'package:podcast_app/application/audio_player/audio_player_events.dart';
+import 'package:podcast_app/models/Podcast.dart';
+import 'package:auto_route/auto_route.dart';
+
 import 'package:podcast_app/presentation/pages/search/widgets/custom_text.dart';
+import 'package:podcast_app/presentation/routes/router.gr.dart';
 
 class Recently extends StatelessWidget {
-  final String title;
-  final String subtitle;
-  final String img;
+  final Podcast podcast;
 
-  const Recently({
-    Key? key,
-    required this.title,
-    required this.subtitle,
-    required this.img,
-  }) : super(key: key);
-
+  const Recently({Key? key, required this.podcast}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    // return Container(
-    //   child: RecentlySearchedItem(img: img, title: title, subtitle: subtitle),
-    // );
+    final audioPlayerBloc = BlocProvider.of<AudioPlayerBloc>(context);
 
-    return Card(
-      elevation: 20,
-      color: HexColor("#282828"),
-      child: ListTile(
-        dense: true,
-        leading: CircleAvatar(
-          backgroundImage: AssetImage(img),
-          radius: 25,
-        ),
-        title: CustomText(text: "$title"),
-        subtitle: CustomText(text: "$subtitle"),
-        trailing: IconButton(
-          onPressed: () {},
-          icon: Icon(
-            Icons.clear,
-            color: Colors.white,
+    return InkWell(
+      onTap: () async {
+        String path = await getDocumentDir();
+        audioPlayerBloc
+            .add(InitializePlayerEvent(podcasts: ListQueue.from([podcast])));
+        context.router.push(PlayerRoute());
+      },
+      child: Card(
+        elevation: 20,
+        color: HexColor("#282828"),
+        child: ListTile(
+          dense: true,
+          leading: CircleAvatar(
+            backgroundImage: AssetImage(podcast.imageUrl!),
+            radius: 25,
+          ),
+          title: CustomText(text: "$podcast.name"),
+          subtitle: CustomText(text: "$podcast.description"),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.clear,
+              color: Colors.white,
+            ),
           ),
         ),
       ),

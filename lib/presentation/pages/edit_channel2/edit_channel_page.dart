c2dd3010b30ae8094cf79132f35692cd26bf86e6
Amanuel Debebe <amanueldebebe1000@gmail.com>
2021-09-09 19:49:57 +0300

    Edit Channel Floating Button Added

--- lib/presentation/pages/edit_channel2/edit_channel_page.dart
+++ lib/presentation/pages/edit_channel2/edit_channel_page.dart
@@ -3,7 +3,8 @@ import 'package:hexcolor/hexcolor.dart';
 import 'package:podcast_app/presentation/pages/core/bottom_nav.dart';
 import 'package:podcast_app/presentation/pages/edit_channel2/widgets/edit_channel_widgets.dart';
 import 'package:podcast_app/presentation/core/bottom_navigation.dart';
-
+import 'package:auto_route/auto_route.dart';
+import 'package:podcast_app/presentation/routes/router.gr.dart';
 import 'widgets/edit_channel_widget.dart';
 
 class EditChannelPage extends StatelessWidget {
@@ -14,6 +15,16 @@ class EditChannelPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: HexColor("#1A1A1A"),
       body: EditChannel(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.router.push(CreatePodcastRoute());
+        },
+        backgroundColor: HexColor("#282828"),
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
     );
   }
 }

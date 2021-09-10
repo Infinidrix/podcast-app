6d01aba483d9a905c067eb340e07bade7907ebf5
Amanuel Debebe <amanueldebebe1000@gmail.com>
2021-09-10 19:30:29 +0300

    Edit Channel Provider

--- lib/data_provider/edit_channel/EditChannelProvider.dart
+++ lib/data_provider/edit_channel/EditChannelProvider.dart
@@ -2,9 +2,11 @@ import 'dart:convert';
 
 import 'package:dartz/dartz.dart';
 import 'package:podcast_app/data_provider/edit_channel/IEditChannelProvider.dart';
+import 'package:podcast_app/data_provider/login/login_provider.dart';
 import 'package:podcast_app/models/Podcast.dart';
 import 'package:podcast_app/models/channel/Channel.dart';
 import 'package:http/http.dart' as http;
+import 'package:podcast_app/data_provider/constants.dart';
 
 class EditChannelProvider extends IEditChannelProvider {
   final http.Client httpClient;
@@ -15,10 +17,15 @@ class EditChannelProvider extends IEditChannelProvider {
   Future<Either<String, Channel>> deletePodcast(
       Podcast podcast, Channel channel) async {
     http.Response response;
+    http.Response response1;
     try {
+      String? userId = await LoginProvider.SESSION.getString('userId');
+
+      response1 = await httpClient.delete(
+          Uri.parse("http://$URL/api/Users/$userId/Audios/${podcast.id}"));
       // TODO:
       response = await httpClient.get(Uri.parse(
-          "'http://192.168.0.131:44343/api/channel/yourchannel/176684b8-461c-41b0-b414-fc0bf49bd8fc'"));
+          "http://$URL/api/Users/$userId/channel/yourchannel/${channel.Id}"));
     } catch (e) {
       return left('Error');
     }
@@ -37,16 +44,27 @@ class EditChannelProvider extends IEditChannelProvider {
     http.Response response;
     http.Response response1;
     try {
-      response = await httpClient.get(Uri.parse(
-          "'http://192.168.0.131:44343/api/channel/yourchannel/176684b8-461c-41b0-b414-fc0bf49bd8fc'"));
+      String? userId = await LoginProvider.SESSION.getString('userId');
+      print("I've got the fucking userId $userId");
+      response1 = await httpClient.patch(
+          Uri.parse("http://$URL/api/Users/$userId/Audios/${podcast.id}"),
+          body: jsonEncode(
+              {"Title": podcast.name, "Description": podcast.description}));
+      response = await httpClient.get(
+          Uri.parse("http://$URL/api/Users/$userId/channel/${channel.Id}"));
     } catch (e) {
+      print("catch error");
       return left("Error");
     }
-    if (response.statusCode == 200) {
+
+    print("response : ${response.statusCode}");
+    print("response1 : ${response1.statusCode}");
+    if (response.statusCode == 200 && response1.statusCode == 200) {
       final parsed = json.decode(response.body);
       print(parsed);
       return Right(Channel.fromJson(parsed));
     } else {
+      print("KEFT");
       return Left("Error");
     }
   }

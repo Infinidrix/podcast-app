36732d6ad67e019e9b37cba5fd67031dd235474f
Hanan Miftah <se.hanan.miftah@gmail.com>
2021-09-10 22:05:33 +0300

    create podcast widget tests

--- test/create_podcast_test/create_podcast_test.dart
+++ test/create_podcast_test/create_podcast_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hexcolor/hexcolor.dart';
 import 'package:podcast_app/application/create_podcast/create_podcast_application.dart';
 import 'package:podcast_app/application/create_podcast/create_podcast_bloc.dart';
 import 'package:podcast_app/data_provider/create_podcast_provider/create_podcast_provider.dart';
@@ -12,19 +13,51 @@ void main() {
     // Create the widget by telling the tester to build it.
     await tester.pumpWidget(
       BlocProvider(
-        create: (context) => CreatePodcastBloc(
-            CreatePodcastInitialState(), CreatePodcastRepository(CreatePodcastProvider())),
+        create: (context) => CreatePodcastBloc(CreatePodcastInitialState(),
+            CreatePodcastRepository(CreatePodcastProvider())),
         child: MaterialApp(
-          home: CreatePodcastWidget(channelId: "",),
+          home: CreatePodcastWidget(
+            channelId: "",
+          ),
         ),
       ),
     );
-
-    // Create the Finders.
     var formFinder = find.byType(Form);
-
-    // Use the `findsOneWidget` matcher provided by flutter_test to
     expect(formFinder, findsOneWidget);
-    // expect(messageFinder, findsOneWidget);
+  });
+
+  testWidgets('CreateChannelWidget contains some widgets',
+      (WidgetTester tester) async {
+    // Create the widget by telling the tester to build it.
+    await tester.pumpWidget(
+      BlocProvider(
+        create: (context) => CreatePodcastBloc(CreatePodcastInitialState(),
+            CreatePodcastRepository(CreatePodcastProvider())),
+        child: MaterialApp(
+          home: CreatePodcastWidget(
+            channelId: "",
+          ),
+        ),
+      ),
+    );
+    var textFinder = find.text("Description");
+    var iconButtons = find.byType(IconButton);
+    var button = find.byType(ElevatedButton);
+    WidgetPredicate widgetSelectedPredicate = (Widget widget) =>
+        widget is DecoratedBox &&
+        widget.decoration ==
+            BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [HexColor("#579EB5"), HexColor("#8EB9C7")]),
+              borderRadius: BorderRadius.circular(20.0),
+            );
+
+    expect(textFinder, findsOneWidget);
+    expect(iconButtons, findsNWidgets(1));
+    expect(button, findsOneWidget);
+    expect(find.byType(ListView), findsNWidgets(1));
+    expect(find.byWidgetPredicate(widgetSelectedPredicate), findsOneWidget);
   });
 }

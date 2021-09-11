b6e16f1c2636f9e441d538f1ee1511fafe31710e
Abdi <se.abdulfeta.dedgba@gmail.com>
2021-09-11 11:47:17 +0300

    fixing test error

--- test/create_podcast_widget_test/create_podcast_test.dart
+++ test/create_podcast_widget_test/create_podcast_test.dart
@@ -7,14 +7,17 @@ import 'package:podcast_app/application/create_podcast/create_podcast_bloc.dart'
 import 'package:podcast_app/data_provider/create_podcast_provider/create_podcast_provider.dart';
 import 'package:podcast_app/presentation/pages/create_podcast/widgets/create_podcast_widget.dart';
 import 'package:podcast_app/repository/CreatePodcastRepository.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   testWidgets('CreateChannelWidget contains Form', (WidgetTester tester) async {
     // Create the widget by telling the tester to build it.
     await tester.pumpWidget(
       BlocProvider(
-        create: (context) => CreatePodcastBloc(CreatePodcastInitialState(),
-            CreatePodcastRepository(CreatePodcastProvider())),
+        create: (context) => CreatePodcastBloc(
+            CreatePodcastInitialState(),
+            CreatePodcastRepository(
+                CreatePodcastProvider(httpClient: http.Client()))),
         child: MaterialApp(
           home: CreatePodcastWidget(
             channelId: "",
@@ -31,8 +34,10 @@ void main() {
     // Create the widget by telling the tester to build it.
     await tester.pumpWidget(
       BlocProvider(
-        create: (context) => CreatePodcastBloc(CreatePodcastInitialState(),
-            CreatePodcastRepository(CreatePodcastProvider())),
+        create: (context) => CreatePodcastBloc(
+            CreatePodcastInitialState(),
+            CreatePodcastRepository(
+                CreatePodcastProvider(httpClient: http.Client()))),
         child: MaterialApp(
           home: CreatePodcastWidget(
             channelId: "",

485a2b17e813e974e2fe40e12d8ea0fe79ba3eac
Hanan Miftah <se.hanan.miftah@gmail.com>
2021-09-11 15:50:54 +0300

    widget tests using mock

--- test/widget_tests/signin_widget_test/signin_widget_test.dart
+++ test/widget_tests/signin_widget_test/signin_widget_test.dart
@@ -5,11 +5,16 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
+import 'package:mocktail/mocktail.dart';
+import 'package:network_image_mock/network_image_mock.dart';
 import 'package:podcast_app/application/home_page/home_page_bloc.dart';
+import 'package:podcast_app/application/home_page/home_page_event.dart';
+import 'package:podcast_app/application/home_page/home_page_state.dart';
 import 'package:podcast_app/application/login/login_bloc.dart';
 import 'package:podcast_app/data_provider/home_page_provider/Home_provider.dart';
 import 'package:podcast_app/data_provider/login/login_provider.dart';
@@ -17,92 +22,105 @@ import 'package:podcast_app/presentation/pages/signin/widgets/signin_page_widget
 import 'package:podcast_app/repository/home_page_repository/HomePageRepository.dart';
 import 'package:podcast_app/repository/login_repository.dart';
 
+class MockSigninBloc extends MockBloc<LoginEvent, LoginState>
+    implements LoginBloc {}
+
+class SigninStateFake extends Fake implements LoginState {}
+
+class SigninEventFake extends Fake implements LoginEvent {}
+
+class MockHomepageBloc extends MockBloc<HomePageEvent, HomePageState>
+    implements HomePageBloc {}
+
+class HomepageStateFake extends Fake implements HomePageState {}
+
+class HomepageEventFake extends Fake implements HomePageEvent {}
+
 void main() {
   final homePageRepository =
       HomePageRepository(dataProvider: HomeProvider(httpClient: http.Client()));
-  testWidgets("Testing the button of signin.", (WidgetTester tester) async {
-    await tester.pumpWidget(
-      MultiBlocProvider(
-        providers: [
-          BlocProvider(
-            create: (context) => LoginBloc(
-                loginRepository: LoginRepository(
-              loginDataProvider: LoginProvider(
-                httpClient: http.Client(),
-              ),
-            )),
-          ),
-          BlocProvider(
-              create: (context) =>
-                  HomePageBloc(repository: homePageRepository)),
-        ],
-        child: MaterialApp(
-          home: SigninWidget(),
-        ),
-      ),
-    );
-    var button = find.text("Sign In");
-    expect(button, findsOneWidget);
-  });
-  testWidgets("Testing the TextFormField.", (WidgetTester tester) async {
-    await tester.pumpWidget(
-      MultiBlocProvider(
-        providers: [
-          BlocProvider(
-            create: (context) => LoginBloc(
-                loginRepository: LoginRepository(
-              loginDataProvider: LoginProvider(
-                httpClient: http.Client(),
-              ),
-            )),
-          ),
-          BlocProvider(
-              create: (context) =>
-                  HomePageBloc(repository: homePageRepository)),
-        ],
-        child: MaterialApp(
-          home: SigninWidget(),
-        ),
-      ),
-    );
-    var emailfield = find.byKey(Key("email_field"));
+  group("Signin widget testing", () {
+    setUpAll(() {
+      registerFallbackValue<LoginState>(SigninStateFake());
+      registerFallbackValue<LoginEvent>(SigninEventFake());
+      registerFallbackValue<HomePageEvent>(HomepageEventFake());
+      registerFallbackValue<HomePageState>(HomepageStateFake());
+    });
 
-    expect(emailfield, findsOneWidget);
-  });
+    testWidgets("Testing the button of signin.", (WidgetTester tester) async {
+      await tester.runAsync(() async {
+        final mockSigninBloc = MockSigninBloc();
+        final mockHomepageBloc = MockHomepageBloc();
 
-  testWidgets("Testing the signin page has some widgets.",
-      (WidgetTester tester) async {
-    await tester.pumpWidget(
-      MultiBlocProvider(
-        providers: [
-          BlocProvider(
-            create: (context) => LoginBloc(
-                loginRepository: LoginRepository(
-              loginDataProvider: LoginProvider(
-                httpClient: http.Client(),
+        when(() => mockSigninBloc.state).thenReturn(
+          InitialLoginState(),
+        );
+        when(() => mockHomepageBloc.state).thenReturn(
+          LoadedHomePageState(topPicks: [], trending: [], recentlyPlayed: []),
+        );
+        final widget = SigninWidget();
+
+        await mockNetworkImagesFor(
+          () async => await tester.pumpWidget(MultiBlocProvider(
+            providers: [
+              BlocProvider<LoginBloc>(
+                create: (context) => mockSigninBloc,
               ),
-            )),
-          ),
-          BlocProvider(
-              create: (context) =>
-                  HomePageBloc(repository: homePageRepository)),
-        ],
-        child: MaterialApp(
-          home: SigninWidget(),
-        ),
-      ),
-    );
-    var noOfContainer = find.byType(Container);
-    WidgetPredicate widgetSelectedPredicate = (Widget widget) =>
-        widget is GestureDetector &&
-        widget.child ==
-            Icon(
-              Icons.remove_red_eye_outlined,
-            );
-    expect(noOfContainer, findsNWidgets(2));
-    expect(find.byWidgetPredicate(widgetSelectedPredicate), findsNothing);
-    expect(_getPasswordTextFormField(), findsOneWidget);
-    expect(_getElevatedButton(), findsNothing);
+              BlocProvider<HomePageBloc>(create: (context) => mockHomepageBloc)
+            ],
+            child: MaterialApp(
+              home: widget,
+            ),
+          )),
+        );
+
+        var button = find.text("Sign In");
+        expect(button, findsOneWidget);
+      });
+    });
+
+    testWidgets("Testing the TextFormField.", (WidgetTester tester) async {
+      await tester.runAsync(() async {
+        final mockSigninBloc = MockSigninBloc();
+        final mockHomepageBloc = MockHomepageBloc();
+
+        when(() => mockSigninBloc.state).thenReturn(
+          InitialLoginState(),
+        );
+        when(() => mockHomepageBloc.state).thenReturn(
+          LoadedHomePageState(topPicks: [], trending: [], recentlyPlayed: []),
+        );
+        final widget = SigninWidget();
+
+        await mockNetworkImagesFor(
+          () async => await tester.pumpWidget(MultiBlocProvider(
+            providers: [
+              BlocProvider<LoginBloc>(
+                create: (context) => mockSigninBloc,
+              ),
+              BlocProvider<HomePageBloc>(create: (context) => mockHomepageBloc)
+            ],
+            child: MaterialApp(
+              home: widget,
+            ),
+          )),
+        );
+
+        var emailfield = find.byKey(Key("email_field"));
+        expect(emailfield, findsOneWidget);
+        var noOfContainer = find.byType(Container);
+        WidgetPredicate widgetSelectedPredicate = (Widget widget) =>
+            widget is GestureDetector &&
+            widget.child ==
+                Icon(
+                  Icons.remove_red_eye_outlined,
+                );
+        expect(noOfContainer, findsNWidgets(2));
+        expect(find.byWidgetPredicate(widgetSelectedPredicate), findsNothing);
+        expect(_getPasswordTextFormField(), findsOneWidget);
+        expect(_getElevatedButton(), findsNothing);
+      });
+    });
   });
 }
 
